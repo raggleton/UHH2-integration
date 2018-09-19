26a4d1e8d8e79db459eabbf84e74a927c6b847f2
Robin Aggleton <robin.aggleton@cern.ch>
2018-09-19 18:09:45 +0200

    Allow for UHH2 repo & branch from any user/org, not just PRs

--- scripts/get_uhh.sh
+++ scripts/get_uhh.sh
@@ -8,11 +8,12 @@ set -u # Don't allow use of unset variables
 mkdir UHH2
 cd UHH2
 git init
-git remote add UHH https://github.com/UHH2/UHH2.git
-git fetch UHH pull/${PRNUM}/head:${PRBRANCH}
-git checkout ${PRBRANCH}
+git remote add UHH https://github.com/${REMOTENAME}/UHH2.git
+git fetch UHH ${REMOTEBRANCH}:${LOCALBRANCH}
+git checkout ${LOCALBRANCH}
 
 # Get the diff for later analysis
+# FIXME: broken if the user isn't testing a PR
 wget -O PR.diff https://patch-diff.githubusercontent.com/raw/UHH2/UHH2/pull/${PRNUM}.diff
 
 set +u
--- to_test.sh
+++ to_test.sh
@@ -1,3 +1,14 @@
-# Only set variables in here to setup UHH2
-export PRNUM=1001
-export PRBRANCH=test${PRNUM}
+# Only set variables in here to get UHH2 from github
+
+# To test a PR, ensure that REMOTENAME is UHH2, set the PR number in PRNUM, 
+# and ensure REMOTEBRANCH has the form: pull/${PRNUM}/head
+#
+# Alternatively, you can set REMOTENAME and REMOTEBRANCH to your 
+# username & branchname to test your branch
+
+export REMOTENAME="UHH2"
+export PRNUM=1008
+export REMOTEBRANCH=pull/${PRNUM}/head
+
+# Local branchname on testmachine, you shouldn't need to touch this
+export LOCALBRANCH="test$PRNUM"
