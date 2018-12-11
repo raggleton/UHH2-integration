36798e7576e345d9b6fe87fda18295548a4610c8
Robin Aggleton <robin.aggleton@cern.ch>
2018-12-11 18:54:31 +0100

    Try force deleting local branch anyway

--- testPR.sh
+++ testPR.sh
@@ -8,13 +8,17 @@
 # 
 # PRID is optional, it will get updated in the CI job if it doesn't exist
 
+set -x # help debugging by printing commands, watch out if you use secrets!
+
 PULLNUM="$1"
 REFBRANCH="$2"
 PRID="$3"
 
 NEWBRANCH="test${PULLNUM}"
+git checkout master
 # If local branch already exists, delete it and do it again
-git rev-parse --verify "${NEWBRANCH}" || git branch -d "${NEWBRANCH}"
+git branch --delete --force "${NEWBRANCH}"
+# git rev-parse --quiet --verify "${NEWBRANCH}" || git branch -d "${NEWBRANCH}"
 git checkout -b "${NEWBRANCH}" master
 NEWFILE="scripts/to_test.sh"
 echo "export PRNUM=${PULLNUM}" >> "${NEWFILE}"
@@ -27,3 +31,5 @@ git commit -m "Test PR ${PULLNUM}"
 git push -f origin "${NEWBRANCH}"  # use force push to overwrite existing branch
 echo "Pushed to origin/${NEWBRANCH}"
 git checkout master
+
+set +x
