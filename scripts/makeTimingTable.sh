6733ace429aab8ad0df2cee1bc8715a94e67713c
Robin Aggleton <robin.aggleton@cern.ch>
2018-12-13 11:38:59 +0100

    Fix issues with spaces and quotes in timing table script

--- scripts/makeTimingTable.sh
+++ scripts/makeTimingTable.sh
@@ -17,9 +17,10 @@ do
         name=$(basename "$newfile")
         name=${name/timing_/}
         name=${name/_new.json/}
-        name=${name//_/ }
+        # name=${name//_/ }  # don't use spaces...seems to cause issues later on passing arg, but only on runner
         headeropt=""
         if [ "$firstline" == true ]; then headeropt="--header"; firstline=false; fi
-        ${CI_PROJECT_DIR}/scripts/timingJsonTable.py  --ref "$reffile" --new "$newfile" --name "$name" "$headeropt" >> "$TIMINGFILE"
+        # Dont use double quotes on headeropt, otherwise runner seems to interpret it badly
+        ${CI_PROJECT_DIR}/scripts/timingJsonTable.py  --ref "$reffile" --new "$newfile" --name "$name" $headeropt >> "$TIMINGFILE"
     fi
 done
