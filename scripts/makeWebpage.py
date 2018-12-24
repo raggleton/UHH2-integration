02155f508b440bd2d1d7c3d326c3b72e9e20a3d5
Robin Aggleton <robin.aggleton@cern.ch>
2018-12-24 01:50:27 +0000

    Add sortable timing tables to webpage, using DataTables
    Use jQuery DataTables to make them sortable,
    pandas to handle dict -> table maniputations & joins

--- scripts/makeWebpage.py
+++ scripts/makeWebpage.py
@@ -9,9 +9,10 @@ from __future__ import print_function
 import os
 import sys
 import json
-from jinja2 import Template, Environment, FileSystemLoader
 import argparse
+import pandas as pd
 from collections import OrderedDict
+from jinja2 import Template, Environment, FileSystemLoader
 try:
     # py2
     from itertools import izip_longest
@@ -72,7 +73,8 @@ def main(in_args):
     parser = argparse.ArgumentParser(description=__doc__)
     parser.add_argument("--plotjson", help="Input plots JSON file", required=True, action='append')
     parser.add_argument("--plotdir", help="Directory with plots in JSON file", required=True, action='append')
-    # parser.add_argument("--timingjson", help="Input timing JSON file", action='append')
+    parser.add_argument("--timingrefjson", help="Input reference timing JSON file", action='append')
+    parser.add_argument("--timingnewjson", help="Input new timing JSON file", action='append')
     # parser.add_argument("--sizejson", help="Input size JSON file", action='append')
     parser.add_argument("--label", help="Label for given plot file.", required=True, action='append')
     parser.add_argument("--outputDir", help="Directory for output directories.", default=".")
@@ -83,20 +85,20 @@ def main(in_args):
         os.makedires(args.outputDir)
 
     file_loader = FileSystemLoader('templates')
-    env = Environment(loader=file_loader)
+    # trim extra newlines around template entries
+    env = Environment(loader=file_loader, trim_blocks=True, lstrip_blocks=True)
 
     template = env.get_template("plotPage.html")
 
     set_of_pages = [(label, os.path.join(args.outputDir, "%s.html" % label.replace(" ", "_")))
                     for label in args.label]
 
-    for plotjson, plotdir, label in zip(args.plotjson, args.plotdir, args.label):
+    for plotjson, plotdir, timing_ref_json, timing_new_json, label in zip(args.plotjson, args.plotdir, args.timingrefjson, args.timingnewjson, args.label):
 
         with open(plotjson) as f:
             orig_plot_data = json.load(f, object_pairs_hook=OrderedDict)
 
         # Do added/remove hists first:
-        added_removed_plots = []
         plot_data = [
                      add_plot_group(group_key=key,
                                     plot_names=orig_plot_data[key]['names'],
@@ -112,6 +114,53 @@ def main(in_args):
                           for key in orig_plot_data['comparison']
                          ])
 
+        # Get timing data
+        with open(timing_ref_json) as f:
+            timing_ref_data = json.load(f, object_pairs_hook=OrderedDict)
+        
+        with open(timing_new_json) as f:
+            timing_new_data = json.load(f, object_pairs_hook=OrderedDict)
+        
+        # Per event timing
+        df_event_timing_new = pd.DataFrame.from_dict(timing_new_data['event_timing'], orient='index')
+        df_event_timing_new.rename(lambda x: 'New', axis='columns', inplace=True)
+        df_event_timing_new.rename(lambda x: x+' [s]', inplace=True)
+        
+        df_event_timing_ref = pd.DataFrame.from_dict(timing_ref_data['event_timing'], orient='index')
+        df_event_timing_ref.rename(lambda x: 'Ref', axis='columns', inplace=True)
+        df_event_timing_ref.rename(lambda x: x+' [s]', inplace=True)
+
+        df_event_timing_diff = df_event_timing_ref.join(df_event_timing_new)
+        df_event_timing_diff['Diff'] = df_event_timing_diff['New'] - df_event_timing_diff['Ref']
+        # df_event_timing_diff = df_event_timing_diff.T
+
+        timing_event_dict = df_event_timing_diff.to_dict(orient='split')
+        timing_event_headers = ['Module name'] + timing_event_dict['columns']
+        timing_event_rows = [[m] + data for m, data in zip(timing_event_dict['index'], timing_event_dict['data'])]
+
+        # Per module timing
+        df_mod_timing_ref = pd.DataFrame.from_dict(timing_ref_data['module_timing'], orient='index')
+        drop_cols = ['per_exec', 'per_visit']
+        df_mod_timing_ref.drop(columns=drop_cols, inplace=True)
+        df_mod_timing_ref.rename(lambda x: x.replace("_", " ").title() + " (Ref) [s]", axis='columns', inplace=True)
+        headers_ref = list(df_mod_timing_ref.columns.values)
+
+        df_mod_timing_new = pd.DataFrame.from_dict(timing_new_data['module_timing'], orient='index')
+        df_mod_timing_new.drop(columns=drop_cols, inplace=True)
+        df_mod_timing_new.rename(lambda x: x.replace("_", " ").title() + " (New) [s]", axis='columns', inplace=True)
+        headers_new = list(df_mod_timing_new.columns.values)
+
+        df_mod_timing_diff = df_mod_timing_ref.join(df_mod_timing_new, how='outer')
+        df_mod_timing_diff = df_mod_timing_diff[~(df_mod_timing_diff==0).all(axis=1)]  # get rid of rows that are all 0s
+        for href, hnew in zip(headers_ref, headers_new):
+            df_mod_timing_diff[href.replace('[s]', '[%]')] = 100. * df_mod_timing_ref[href] / df_mod_timing_ref[href].sum(skipna=True)
+            df_mod_timing_diff[hnew.replace('[s]', '[%]')] = 100. * df_mod_timing_new[hnew] / df_mod_timing_new[hnew].sum(skipna=True)
+            df_mod_timing_diff['Diff '+href.replace(" (Ref)", "")] = df_mod_timing_new[hnew] - df_mod_timing_ref[href]
+
+        timing_mod_dict = df_mod_timing_diff.to_dict(orient='split')
+        timing_mod_headers = ['Module name'] + timing_mod_dict['columns']
+        timing_mod_rows = [[m] + data for m, data in zip(timing_mod_dict['index'], timing_mod_dict['data'])]
+
         # Render template with objects
         prnum = os.environ.get("PRNUM", -1)
         gitlab_url = os.environ.get("CI_PIPELINE_URL", "https://gitlab.cern.ch/raggleto/UHH2-integration/pipelines")
@@ -119,7 +168,11 @@ def main(in_args):
                                  gitlab_url=gitlab_url,
                                  num_plots=orig_plot_data['total_number'],
                                  plot_data=plot_data,
-                                 set_of_pages=set_of_pages
+                                 set_of_pages=set_of_pages,
+                                 timing_event_headers=timing_event_headers,
+                                 timing_event_rows=timing_event_rows,
+                                 timing_mod_headers=timing_mod_headers,
+                                 timing_mod_rows=timing_mod_rows,
                                 )
 
         html_filename = os.path.join(args.outputDir, "%s.html" % label.replace(" ", "_"))
