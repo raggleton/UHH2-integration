190277030ac3a853bab9bd5306d99136073f2dda
Robin Aggleton <robin.aggleton@cern.ch>
2019-01-01 21:38:24 +0000

    Add script to make webpages from samples

---
+++ scripts/makeAllWebpages.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/env bash
+#
+# Do comparison plots, & compile all analysis info into webpage
+
+args=""
+for newfile in ${TESTDIR}/Ntuple*_new.root;
+do
+    echo $newfile
+    reffile=${newfile/_new.root/_ref.root}
+    if [ -f "$reffile" ]; then
+        # Get sample name from filename
+        name=$(basename "$newfile")
+        name=${name/Ntuple_/}
+        name=${name/_new.root/}
+        "plots_${name}"
+        args="$args --plotsjson plots_${name}.json --plotdir plots_${name} \
+                    --timingrefjson timing_${name}_ref.json --timingnewjson timing_${name}_new.json \
+                    --sizerefjson size_${name}_ref.json --sizenewjson size_${name}_new.json \
+                    --label ${name}"
+    else
+        echo "Cannot find matching file $reffile"
+    fi
+done
+echo "args: "$args
+
+# Need to put website contents in directory "public"
+export WEBEND="UHH2integration/${LOCALBRANCH}"
+ODIR=${CI_PROJECT_DIR}/web/${WEBEND}
+mkdir -p $ODIR
+python ${CI_PROJECT_DIR}/scripts/makeWebpage.py --outputDir "$ODIR" $args
