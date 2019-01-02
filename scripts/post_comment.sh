1152f40b9633e83c034ef9b3dc4d9d3bd6ba71c6
Robin Aggleton <robin.aggleton@cern.ch>
2019-01-02 14:48:20 +0000

    Add var to not mute github comments

--- scripts/post_comment.sh
+++ scripts/post_comment.sh
@@ -1,13 +1,24 @@
 #!/user/bin/env bash
 
 set +x  # Otherwise our token will get exposed!
+set +u  # Below tests might fail otherwise
 
 # Post a comment to Github
 # Requires the following envvars to be set:
 # - GITHUB_TOKEN
 # - PRID
 #
+# Setting GITHUB_QUIET=1 will not send any comments to github.
+# To post, unset or set to 0
+#
 # Usage:
 # ./post_comment.sh "Blah blah blah"
-QUERY="mutation { addComment(input: { subjectId: \\\"${PRID}\\\", clientMutationId: \\\"integration-test\\\", body: \\\"${1}\\\"}) { clientMutationId } }"
-curl -H "Authorization: bearer ${GITHUB_TOKEN}" -X POST -d "{ \"query\":\"$QUERY\"}" https://api.github.com/graphql -s -o /dev/null
+
+if [ -n "$GITHUB_QUIET" ] && [ "$GITHUB_QUIET" -ne 0 ]
+then
+    # don't post
+    return 0
+else
+    QUERY="mutation { addComment(input: { subjectId: \\\"${PRID}\\\", clientMutationId: \\\"integration-test\\\", body: \\\"${1}\\\"}) { clientMutationId } }"
+    curl -H "Authorization: bearer ${GITHUB_TOKEN}" -X POST -d "{ \"query\":\"$QUERY\"}" https://api.github.com/graphql -s -o /dev/null
+fi
