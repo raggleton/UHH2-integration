7b90a7bebcf7d476cf3b1b066ccddf115d80569f
Robin Aggleton <robin.aggleton@cern.ch>
2019-03-06 17:42:24 +0100

    Fix time command

--- scripts/post_install.sh
+++ scripts/post_install.sh
@@ -8,9 +8,9 @@ ulimit -a
 # Compile SFrame and UHH
 cd ${CMSSW_BASE}/../SFrame
 source setup.sh
-/usr/bin/time -v make ${MAKEFLAGS}
+time -v make ${MAKEFLAGS}
 cd ${CMSSW_BASE}/src/UHH2
-/usr/bin/time -v make ${MAKEFLAGS}
+time -v make ${MAKEFLAGS}
 
 # Hack to make cmsRun work on the images as no default site set
 export CMS_PATH=/cvmfs/cms-ib.cern.ch/
